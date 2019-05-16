6c4509db495d1fb97e4a32cd0ab03bcad5e319b1
Will Murchison <wmurch@hotmail.com>
2019-05-16 17:00:02 -0400

    added image to fetch for background

--- public/main.js
+++ public/main.js
@@ -1,7 +1,18 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = ''
-  }
+  imageUrl = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+  fetch(imageUrl)
+    .then(response => {
+      console.log('2')
+
+      console.log(response)
+      return response.json()
+    })
+    .then(headerImage => {
+      document.querySelector('header').style.backgroundImage =
+        'url(headerImage.hdUrl)'
+
+      console.log(headerImage)
+    })
 }
 
 document.addEventListener('DOMContentLoaded', main)
