8b8b0dbd62ef37f36057b03a08015fd7d25118e6
Will Murchison <wmurch@hotmail.com>
2019-05-17 14:05:32 -0400

    added fetch for Launch details

--- public/main.js
+++ public/main.js
@@ -1,17 +1,45 @@
+//store NASA updates in an array
+
+//load and display carousel
+const updateCarousel = () => {
+  document.querySelector
+}
 const main = () => {
-  imageUrl = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
-  fetch(imageUrl)
+  //fetch pic of the day
+  picOfTheDay = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+  fetch(picOfTheDay)
     .then(response => {
-      console.log('2')
+      console.log('Nasa Image')
 
       console.log(response)
       return response.json()
     })
     .then(headerImage => {
-      document.querySelector('header').style.backgroundImage =
+      document.querySelector('.header-center').style.backgroundImage =
         'url(' + headerImage.hdUrl + ')'
 
       console.log(headerImage)
+    })
+  //fetch Nasa launch data
+  nasaLaunch =
+    'https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming'
+  fetch(nasaLaunch)
+    .then(response => {
+      console.log('launch times')
+
+      console.log(response)
+      return response.json()
+    })
+    .then(launchContent => {
+      console.log({ launchContent })
+      document.querySelector('.launch-title').textContent =
+        launchContent[0].mission_name
+      document.querySelector('.launch-details').textContent =
+        launchContent[0].details
+      document.querySelector('.launch-status').textContent =
+        launchContent[0].launch_success
+      document.querySelector('.launch-location').textContent =
+        launchContent[0].launch_site.site_name_long
     })
 }
 
