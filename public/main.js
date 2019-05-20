af1ed9edc2f2496aa176ab1b753a05e9cb6dffe5
Will Murchison <wmurch@hotmail.com>
2019-05-19 20:35:15 -0400

    stored all fetched items into an array

--- public/main.js
+++ public/main.js
@@ -1,46 +1,94 @@
 //store NASA updates in an array
-
+position = 0
+articles = []
 //load and display carousel
 const updateCarousel = () => {
-  document.querySelector
+  // console.log('Update Carousel')
+  document.querySelector('.launch-title').textContent = articles[position].title
+  document.querySelector('.launch-details').textContent =
+    articles[position].details
+  document.querySelector('.launch-status').textContent =
+    articles[position].status
+  document.querySelector('.launch-location').textContent =
+    articles[position].location
 }
 const main = () => {
   //fetch pic of the day
   picOfTheDay = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
   fetch(picOfTheDay)
     .then(response => {
-      console.log('Nasa Image')
+      // console.log('Nasa Image')
 
-      console.log(response)
+      //  console.log(response)
       return response.json()
     })
     .then(headerImage => {
       document.querySelector('.header-center').style.backgroundImage =
         'url(' + headerImage.hdUrl + ')'
 
-      console.log({ headerImage })
+      //    console.log({ headerImage })
     })
   //fetch Nasa launch data
   nasaLaunch =
     'https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming'
   fetch(nasaLaunch)
     .then(response => {
-      console.log('launch times')
+      //   console.log('launch times')
 
-      console.log(response)
+      // console.log(response)
       return response.json()
     })
-    .then(launchContent => {
-      console.log({ launchContent })
-      document.querySelector('.launch-title').textContent =
-        launchContent[0].mission_name
-      document.querySelector('.launch-details').textContent =
-        launchContent[0].details
-      document.querySelector('.launch-status').textContent =
-        launchContent[0].launch_success
-      document.querySelector('.launch-location').textContent =
-        launchContent[0].launch_site.site_name_long
+    .then(launchArticles => {
+      // console.log({ launchArticles })
+      for (position = 0; position < launchArticles.length; position++) {
+        const article = {
+          title: launchArticles[position].mission_name,
+          details: launchArticles[position].details,
+          status: launchArticles[position].launch_success,
+          location: launchArticles[position].launch_site.site_name_long
+        }
+        position = 0
+        articles.push(article)
+        updateCarousel()
+      }
     })
+}
+console.log(articles)
+const loadPrevCarousel = () => {
+  console.log('Get Previous Carousel')
+  prev = position--
+  if ((prev = -1)) {
+    return launchArticles.length--
+  } else {
+    console.log(prev)
+    /* document.querySelector('.launch-title').textContent =
+        launchArticles[position].title
+      document.querySelector('.launch-details').textContent =
+        launchArticles[position].details
+      document.querySelector('.launch-status').textContent =
+        launchArticles[position].status
+      document.querySelector('.launch-location').textContent =
+        launchArticles[position].location */
+  }
+}
+const loadNextCarousel = () => {
+  console.log('Get Next Carousel')
+  next = position++
+  if ((next = launchArticles.length)) {
+    return 0
+  } else {
+    console.log(next)
+    /* document.querySelector('.launch-title').textContent =
+        launchArticles[position].title
+      document.querySelector('.launch-details').textContent =
+        launchArticles[position].details
+      document.querySelector('.launch-status').textContent =
+        launchArticles[position].status
+      document.querySelector('.launch-location').textContent =
+        launchArticles[position].location */
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.prev').addEventListener('click', loadPrevCarousel)
+document.querySelector('.next').addEventListener('click', loadNextCarousel)
